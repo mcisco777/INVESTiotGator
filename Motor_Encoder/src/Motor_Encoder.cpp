b42e88fac3113e75481fe8c8b5afd35f6747bab5
Mario Cisneros <macisco777@msn.com>
2026-04-13 17:36:44 -0600

    Encoder EOD commit

--- Motor_Encoder/src/Motor_Encoder.cpp
+++ Motor_Encoder/src/Motor_Encoder.cpp
@@ -17,10 +17,10 @@ const int BATT_ADDR = 0x00;
 const int POLARITY_ADDR = 0x15;
 const int PWM_ADDR = 0x1F;
 const int FIXED_SPEED_ADDR = 0x33;
-const int ENC_TOTAL_ADDR = 0x3C
-const int MT_JGB37 = 3;
+const int ENC_TOTAL_ADDR = 0x3C;
+const int MOTOR_TYPE_JGB37_520_12V_110RPM = 3;
 
-int8_t MotorType = MT_JGB37;
+int8_t MotorType = MOTOR_TYPE_JGB37_520_12V_110RPM;
 int8_t MotorPolarity = 0;
 
 int8_t car_forward[4]={0,23,0,-23};
@@ -29,10 +29,12 @@ int8_t car_stop[4]={0,0,0,0};
 
 /*********************Declare Functions*******************/
 //bool WireWriteByte(uint8_t val);   //(Send byte data through I2C)
-bool WireWriteDataArray(  uint8_t reg,uint8_t *val,unsigned int len);    //(Send data through I2C)
+bool WireWriteDataArray(uint8_t reg,int8_t *val,unsigned int len);    //(Send data through I2C)
 
 
 void setup() {
+  Serial.begin(9600);
+  waitFor(Serial.isConnected, 5000);
   Wire.begin();
   delay(200);
   WireWriteDataArray(MOTOR_TYPE_ADDR,&MotorType,1);
@@ -42,19 +44,18 @@ void setup() {
 
 // loop() runs over and over again, as quickly as it can execute.
 void loop() {
-  WireWriteDataArray(PWM_ADDR,forward,2);
-   delay(2000);
+  WireWriteDataArray(FIXED_SPEED_ADDR,car_forward,4);
+  delay(2000);
+  WireWriteDataArray(FIXED_SPEED_ADDR,car_stop,4);
+  delay(1000);
+  /* (Car moves backward) */
+  WireWriteDataArray(FIXED_SPEED_ADDR,car_retreat,4);
+  delay(2000);
+  WireWriteDataArray(FIXED_SPEED_ADDR,car_stop,4);
+  delay(1000);  
 }
 
-bool WireWriteByte(uint8_t val){   //(Send byte data through I2C)
-    Wire.beginTransmission(ENC_ADDR);
-    Wire.write(val);
-    if( Wire.endTransmission() != 0 ) {
-        return false;
-    }
-    return true;
-}
-bool WireWriteDataArray(  uint8_t reg,uint8_t *val,unsigned int len){    //(Send data through I2C)
+bool WireWriteDataArray(uint8_t reg,int8_t *val,unsigned int len){    //(Send data through I2C)
     unsigned int i;
 
     Wire.beginTransmission(ENC_ADDR);   //(Set the starting address of the task)
@@ -68,3 +69,12 @@ bool WireWriteDataArray(  uint8_t reg,uint8_t *val,unsigned int len){    //(Send
 
     return true;
 }
+
+/*bool WireWriteByte(uint8_t val){   //(Send byte data through I2C)
+    Wire.beginTransmission(ENC_ADDR);
+    Wire.write(val);
+    if( Wire.endTransmission() != 0 ) {
+        return false;
+    }
+    return true;
+}*/
