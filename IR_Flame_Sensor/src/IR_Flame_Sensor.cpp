b31fc57eefdb98ee383e10210658b74b2542043c
Mario Cisneros <macisco777@msn.com>
2026-04-14 11:43:33 -0600

    IR Flame sensor analog

--- IR_Flame_Sensor/src/IR_Flame_Sensor.cpp
+++ IR_Flame_Sensor/src/IR_Flame_Sensor.cpp
@@ -12,28 +12,33 @@ SYSTEM_MODE(SEMI_AUTOMATIC);
 SYSTEM_THREAD(ENABLED);
 
 
-const int DO_PIN = D2; // P2's pin connected to DO pin of the flame sensor
-
+//const int DO_PIN = D2; // P2's pin connected to DO pin of the flame sensor
+const int AO_PIN = A0;  // P2's pin connected to AO pin of the flame sensor
 
 void setup() {
   // initialize serial communication
   Serial.begin(9600);
-  // initialize the Arduino's pin as an input
-  pinMode(DO_PIN, INPUT);
+  // initialize the P2's pin as an input for digital pin use
+  /*pinMode(DO_PIN, INPUT);*/
 }
 
 void loop() {
-  int flame_state = digitalRead(DO_PIN);
+  /*int flame_state = digitalRead(DO_PIN);
 
   if (flame_state == HIGH)
     Serial.printf("The flame is NOT present => The fire is NOT detected\n");
   else
     Serial.printf("The flame is present => The fire is detected\n");
+    */
+   
+  int flameValue = analogRead(AO_PIN); // value of ~ 3900 when the flame is not present and ~ 200 when the flame is present
+
+  Serial.printf("Flame Value: %d\n", flameValue);
 }
 
 
 /*
-#define AO_PIN A0  // Arduino's pin connected to AO pin of the flame sensor
+const int AO_PIN = A0;  // P2's pin connected to AO pin of the flame sensor
 
 void setup() {
   // initialize serial communication
